3d0fa5c6b061c62b515d43310cdb947129197c8b
Douglas dos Santos <douglas.astudy@gmail.com>
2023-01-20 20:38:38 -0300

    Update modulo.py

--- POO Python/Cadastro/modulo.py
+++ POO Python/Cadastro/modulo.py
@@ -1,4 +1,23 @@
 def nome():
     try:
         nome = str(input('Digite seu nome:'))
+    except:
         
+        
+
+def JuntarListas(lista1, lista2):
+    try:
+        lista = []
+        dados= []
+        for c in range(0, len(lista1)):
+            dados.append(lista1[c])
+            dados.append(lista2[c])
+            lista.append(dados[:])
+            dados.clear()
+    except:
+        print('Erro, as listas não são do mesmo tamanho.')
+    else:
+        return lista
+
+        
+    
