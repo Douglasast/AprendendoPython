e6a27f28a110fde7fec05d9ca9d9b0355868e24d
Douglas dos Santos <douglas.astudy@gmail.com>
2023-02-02 19:53:38 -0300

    update

--- Mini Projetos/Automação de planilha de vendas Excel HashtagProgramação/main.py
+++ Mini Projetos/Automação de planilha de vendas Excel HashtagProgramação/main.py
@@ -3,8 +3,9 @@ import pandas as pd
 lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
 
 for mes in lista_meses:
-    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    tabela_vendas = pd.read_excel(f'C:/Users/Backup/Documents/GitHub/AprendendoPython/Mini Projetos/Automação de planilha de vendas Excel HashtagProgramação/{mes}.xlsx')
     if (tabela_vendas['Vendas'] > 55000).any():
         vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000,'Vendedor'].values[0]
         vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000,'Vendas'].values[0]
         print(f'No mês de {mes} o vendedor {vendedor} atingiu a meta de R$55.000, conseguindo: R${vendas}')
+
--- Mini Projetos/Teste Pandas/main.py
+++ Mini Projetos/Teste Pandas/main.py
@@ -1,3 +1,56 @@
 import pandas as pd
-planilha = pd.read_excel(r'C:\Users\Backup\Documents\GitHub\AprendendoPython\Mini Projetos\Teste Pandas\Abril.xlsx')
-print(planilha['Vendas'])
+
+#criando um dataframe vazio:
+df = pd.DataFrame()
+
+
+#Criando um dataframe usando um dicionário:
+Pessoa = {'Nome': ['Willow', 'Wilson'], 
+        'Idade': [19, 21],
+        'Nacionalidade': ['Constant','Constant'],}
+Tabela_pessoa = pd.DataFrame(Pessoa)
+
+
+#Mostrando um dataframe: 
+print(Tabela_pessoa)
+
+
+#Criando e mostrando dataframe apenas com a colunas importantes e na ordem desejada
+Pessoa = {'Nome': ['Willow', 'Wilson'], 
+        'Idade': [19, 21],
+        'Nacionalidade': ['Constant','Constant'],}
+Tabela_pessoa = pd.DataFrame(Pessoa, columns=['Nacionalidade','Nome'])
+print(Tabela_pessoa)
+
+
+#Lendo um arquivo Excel/.xlsx:
+planilha_df = pd.read_excel('C:/Users/Backup/Documents/GitHub/AprendendoPython/Mini Projetos/Teste Pandas/Abril.xlsx')
+
+
+#Mostrando um dado do arquivo, [Coluna][linha]:
+print(planilha_df['Vendas'][8])
+
+
+#mostrando apenas o inicio do DataFrame:
+print(planilha_df.head(10))
+
+#mostrando o número de colunas e linhas:
+print(planilha_df.shape)
+
+
+#Lendo uma tabela de um banco de dados SQL:
+import pandas as pd
+import mysql.connector
+
+
+conexão = mysql.connector.connect(
+  host="hostname",
+  user="nome de usuário",
+  password="senha",
+  database="nome do banco de dados")
+tabela_sql = pd.read_sql_query('SELECT * FROM tabela', conexão)
+
+print(tabela_sql)
+
+
+
